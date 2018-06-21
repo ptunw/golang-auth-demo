34ba418b62eb23caac0d1bbed5c7166b1c90890f
marvin <marvintpa.never@gmail.com>
2018-06-21 23:46:49 +0530

    work-in-progress

--- lib/user/dao.go
+++ lib/user/dao.go
@@ -1,1 +1,23 @@
 package user
+
+import (
+	"log"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type UserDao struct {
+	Server   string
+	Database string
+}
+
+var db *mgo.Database
+
+func (u *UserDao) Connect() {
+	session, err := mgo.Dial(u.Server)
+	if err != nil {
+
+		log.Fatal(err)
+	}
+	db = session.DB(u.Database)
+}
