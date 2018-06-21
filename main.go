77c8a9e1f25c4ffb9818a59c0cd6e66cb90f2b22
marvin <marvintpa.never@gmail.com>
2018-06-21 11:27:53 +0530

    wip

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/json"
+    "encoding/json"
     "log"
     "net/http"
     "github.com/gorilla/mux"
@@ -9,31 +9,58 @@ import (
 )
 
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/login", LoginUser).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8000", router))
+    insecureRouter := mux.NewRouter()
+    secureRouter := mux.NewRouter()
+    amw := authMiddleWare{}
+    secureRouter.Use(amw.ValidateJWT)
+    insecureRouter.HandleFunc("/login", LoginUser).Methods("POST")
+    secureRouter.HandleFunc("/me", FetchLoggedInUser).Methods("GET")
+    log.Fatal(http.ListenAndServe(":8000", insecureRouter, secureRouter))
 }
 
 type User struct {
-	Email string `json:"email"`
-	Name string `json:"name"`
+    Email string `json:"email"`
+    Name string `json:"name"`
 }
 
 type Credential struct {
-	Email string `json:"email"`
-	Password string `json: "password"`
+    Email string `json:"email"`
+    Password string `json: "password"`
 }
 
 func LoginUser(w http.ResponseWriter, r *http.Request) {
-	jsonDecoder := json.NewDecoder(r.Body)
-	var cred Credential
-	err := jsonDecoder.Decode(&cred)
-	if err != nil {
-		panic(err)
-	}
-	const validEmail = "ak.sskg@gmail.com"
-	const validPassword = "password"
-	if cred.Email == validEmail && cred.Password == validPassword {
-		w.Write([]byte("head.pay.sign"))
-	}
+    jsonDecoder := json.NewDecoder(r.Body)
+    var cred Credential
+    err := jsonDecoder.Decode(&cred)
+    if err != nil {
+        panic(err)
+    }
+    //These values should come from database
+    const validEmail = "ak.sskg@gmail.com"
+    const validPassword = "password" //password should have been hashed
+    if cred.Email == validEmail && cred.Password == validPassword {
+        w.Write([]byte("head.pay.sign"))
+    }
+}
+
+func FetchLoggedInUser(w http.ResponseWriter, r *http.Request) {
+    
+}
+
+
+type authMiddleWare struct {
+
+}
+
+func (amw *authMiddleWare) ValidateJWT(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        //token := r.Header.Get("Authorization")
+
+        //if valid user {
+            next.ServeHTTP(w, r)
+        //} else {
+            
+            //http.Error(w, "Forbidden", http.StatusForbidden)
+        //}
+    })
 }
