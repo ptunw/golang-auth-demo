ac1237f2c798a09c4148e5baf9e93843d4e4b2c7
marvin <marvintpa.never@gmail.com>
2018-06-21 16:51:04 +0530

    jwt token and user fetch for single hardcoded user

--- main.go
+++ main.go
@@ -5,17 +5,18 @@ import (
     "log"
     "net/http"
     "github.com/gorilla/mux"
-    //"github.com/dgrijalva/jwt-go"
+    "github.com/dgrijalva/jwt-go"
+    "github.com/gorilla/context"
+    "strings"
 )
 
 func main() {
-    insecureRouter := mux.NewRouter()
-    secureRouter := mux.NewRouter()
+    router := mux.NewRouter()
     amw := authMiddleWare{}
-    secureRouter.Use(amw.ValidateJWT)
-    insecureRouter.HandleFunc("/login", LoginUser).Methods("POST")
-    secureRouter.HandleFunc("/me", FetchLoggedInUser).Methods("GET")
-    log.Fatal(http.ListenAndServe(":8000", insecureRouter, secureRouter))
+    router.Use(amw.ValidateJWT)
+    router.HandleFunc("/login", LoginUser).Methods("POST")
+    router.HandleFunc("/me", FetchLoggedInUser).Methods("GET")
+    log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 type User struct {
@@ -35,16 +36,26 @@ func LoginUser(w http.ResponseWriter, r *http.Request) {
     if err != nil {
         panic(err)
     }
+
     //These values should come from database
     const validEmail = "ak.sskg@gmail.com"
     const validPassword = "password" //password should have been hashed
     if cred.Email == validEmail && cred.Password == validPassword {
-        w.Write([]byte("head.pay.sign"))
+        token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{ "email": cred.Email })
+        tokenString, error := token.SignedString([]byte("mysecretkey"))
+        if error != nil {
+            panic(error)
+        }
+        w.Write([]byte(tokenString))
     }
 }
 
 func FetchLoggedInUser(w http.ResponseWriter, r *http.Request) {
     
+    loggedInUserEmail := context.Get(r, "email").(string)
+    u := &User{Email: loggedInUserEmail, Name: "Unknown"}
+    umarshalled, _ := json.Marshal(u)
+    w.Write(umarshalled)
 }
 
 
@@ -54,13 +65,26 @@ type authMiddleWare struct {
 
 func (amw *authMiddleWare) ValidateJWT(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        //token := r.Header.Get("Authorization")
+        token := r.Header.Get("Authorization")
+        if token == "" &&  r.URL.Path == "/login" {
+            LoginUser(w, r)
+        } else {
+            tokenToken := strings.Split(token, " ")
+            if len(tokenToken) == 2 {
+                parsed, error := jwt.Parse(tokenToken[1], func(token *jwt.Token) (interface{}, error) {
+                    return []byte("mysecretkey"), nil
+                })
 
-        //if valid user {
-            next.ServeHTTP(w, r)
-        //} else {
-            
-            //http.Error(w, "Forbidden", http.StatusForbidden)
-        //}
+                if error != nil {
+                    log.Println(error)
+                    w.WriteHeader(http.StatusUnauthorized)
+
+                } else {
+                    c := parsed.Claims.(jwt.MapClaims)
+                    context.Set(r, "email", c["email"])
+                    next.ServeHTTP(w, r)
+                }
+            }
+        }
     })
 }
